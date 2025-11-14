1accdc5e41410f5c1b8a3f603e9c614e1875c77b
Kaamibroken <kaamibroken64@gmail.com>
2025-11-14 15:14:22 +0500

    Update database.js

--- database.js
+++ database.js
@@ -1,12 +1,10 @@
 import express from "express";
 import fetch from "node-fetch";
-import cors from "cors";
 
 const app = express();
-app.use(cors()); // allow frontend
 
 app.get("/", (req, res) => {
-  res.send("Kami Flex API Working");
+  res.send("✅ Kami Flex SIM Database API is working!");
 });
 
 app.get("/api/search", async (req, res) => {
@@ -15,14 +13,11 @@ app.get("/api/search", async (req, res) => {
     if (!phone) {
       return res.status(400).json({ error: "Phone number is required" });
     }
-
-    const url = `https://api.impossible-world.xyz/api/data?phone=${phone}`;
-    const response = await fetch(url);
+    const response = await fetch(`https://api.impossible-world.xyz/api/data?phone=${phone}`);
     const data = await response.json();
-
     res.json(data);
   } catch (err) {
-    res.status(500).json({ error: "Server error", details: err.message });
+    res.status(500).json({ error: "Server Error", details: err.message });
   }
 });
 
